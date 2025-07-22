09f9345640468b761c2d88e85b69c8c1cb6ea0f6
JivadeDnyaneshwar <jivadednyaneshwar7@gmail.com>
2025-07-22 17:29:42 +0530

    Updated test.py

---
+++ TwoSum.py
@@ -0,0 +1,50 @@
+#  #Swap Two Numbers Without Using a Third Variable
+# a=10
+# b=7
+# a=a+b
+# b=a-b
+# a=a-b
+# print("a:",a)
+# print("b:",b)
+
+ #Calculate the Area of a Circle
+# radius=5
+# area=3.14*radius*radius
+# print("Calculate the Area of a Circle:",area)
+
+
+#  Check if a Number is Positive, Negative, or Zero
+
+# num=int(input("Enter the Number:"))
+# if num > 0:
+#     print("Number is Positive:")
+# elif num == 0:
+#     print("Number is Zero:")
+# else:
+#     print("Number is Negative:")    
+
+
+#  Identify the Character Type
+
+# char=input("Enter the Charactor:")
+# if char.isalpha():
+#     if char.lower()in'aeiou':
+#         print("Vowel")
+#     else:
+#         print("Consonent")
+# elif char.isdigit():
+#     print("Digit")
+# else:
+#     print("Special Char")
+
+
+#  Find the Smallest of Three Numbers
+
+# sum of number
+sum=int(input("Enter the Number:"))
+s=0
+i=1
+while i<=sum:
+    s+=i
+    i+=1
+    print("Sum",s)
